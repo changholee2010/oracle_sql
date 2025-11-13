3532c797eff88b8a11489f2a2148f6436747f1cf
world <hello@email.com>
2025-11-13 17:51:29 +0900

    sql

--- Scott.sql
+++ Scott.sql
@@ -48,3 +48,104 @@ where sal + nvl(comm, 0) >= 1500--&sal
 order by comm; --or sal >= 2000; -- 급여가 2000이 넘는 사람들.
 -- 급여  / 급여 + 보너스 => 급여.
 
+-- 집합연산자(union)
+select studno, name, deptno1, 1 as gubun
+from student s
+union all
+select profno, name, deptno, 2
+from professor;
+
+select studno, name--, deptno1
+from student
+where  deptno1 = 101
+minus
+select studno, name--, deptno2
+from student
+where deptno2 = 201
+order by 1;
+
+select *
+from professor;
+
+-- 단일행함수.
+select substr(ename,1,2) as "Name"
+from emp;
+-- 복수행함수.
+select count(*)
+from emp; -- 14건
+
+select name
+      ,tel
+      ,substr(tel, 1, instr(tel, ')')-1) as "AREA CODE"
+from student
+where deptno1 = 201;
+
+select lpad(ename, 9, '123456789') as "LPAD"
+      ,rpad(ename, 9, substr('123456789', length(ename)+1)) as "RPAD"
+      ,substr('123456789', length(ename)+1) as "SUB"
+      ,length(ename)+1 as "LENGTH"
+from emp
+where deptno = 10;
+
+select replace(ename, substr(ename, 1, 2), '**') as "REPLACE"
+from emp
+where deptno = 10;
+-- Quiz.1
+select ename
+      ,replace(ename, substr(ename, 2,2), '--') as "REPLACE"
+from emp
+where deptno = 20;
+-- Quiz.2
+select name
+      ,jumin
+      ,replace(jumin, substr(jumin,7,8), '-/-/-/-/') as "REPLACE"
+from student
+where deptno1 = 101;
+-- Quiz.3
+select name
+      ,tel
+      ,replace(tel, substr(tel, 5,3), '***') as "REPLACE"
+from student
+where deptno1 = 102;
+-- Quiz.4
+select name
+      ,tel
+      ,replace(tel, substr(tel, instr(tel, '-')+1, 4), '****') as "REPLACE"
+from student
+where deptno1 = 101;
+
+select round(12.12345, 4) round
+      ,trunc(12.12345, 2) trunc
+      ,mod(12, 10) mod
+      ,ceil(12.12345) ceil
+      ,floor(12.12345) floor
+      ,power(2,2) power
+from dual;
+-- char(5) '  abc'
+-- varchar2(5) 'abc'
+-- number(5, 3) 12.345, 12.345
+-- date 2025-11-13 >= '00-01-01'
+-- cdata, bdata, bfile => 
+select sysdate
+      ,months_between('2025/02/01', '2025/01/01') bet
+      ,add_months(sysdate, -1) add_month
+      ,sysdate + 31 add_month2
+      ,next_day(sysdate, '금') nextday
+      ,last_day(add_months(sysdate,1)) last_day
+      ,to_char(round(sysdate), 'rrrr/mm/dd hh24:mi:ss') roound
+      ,to_char(trunc(sysdate), 'rrrr/mm/dd hh24:mi:ss') trunc
+from dual;
+
+select to_char(1234, '09999') to_char
+      ,to_char(1234.23, '9999.99') num
+      ,to_char(123456789, '999,999,999') format
+from dual;
+
+-- commission 계산.
+select empno
+      ,ename
+      ,to_char(hiredate, 'rrrr-mm-dd') hiredate
+      ,to_char((sal*12)+comm, '$99,999') sal
+      ,to_char((sal*12+comm)*1.15, '$99,999') "15% UP"
+from emp
+where comm is not null;
--- 인사관리.sql
+++ 인사관리.sql
@@ -10,3 +10,31 @@ SELECT employee_id
      , hire_date
 FROM employees -- 테이블.
 ;
+
+select *
+from employees;
+
+-- 1. 입사일자 :2000년 이후인 사원조회.
+SELECT *
+FROM employees
+where hire_date >= '00/01/01';
+-- 2. salary 가 10000인 이상인데 2000년대인 사원.
+SELECT *
+FROM employees
+WHERE salary >= 10000
+AND   hire_date >= '00/01/01';
+-- 3. 이름(first)이 5글자인 사원
+SELECT *
+FROM employees
+WHERE first_name like '_____';
+-- 4. 급여(salary, salary+보너스)의 10000 이상인 사원.
+SELECT *
+FROM employees
+WHERE salary >= 10000
+or (salary + salary * commission_pct) >= 10000;
+
+--
+select first_name
+      ,ltrim(first_name, 'E') as "ltrim"
+      ,replace(rpad(first_name, 10, '*'), '*', '-') as "Lpad"
+from employees;
